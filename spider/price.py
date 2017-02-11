7c5b08c912d8cc15a66daaa9a461ed1a414a6fac
KoiKorvin <koikorvin@163.com>
2017-02-11 13:53:28 +0800

    爬虫

--- spider/price.py
+++ spider/price.py
@@ -1,9 +1,12 @@
+import re
 import urllib.request
 
-date='2017-3-23'
-url='http://flights.ctrip.com/booking/WNZ-HGH-day-1.html?DDate1='+date
+from builtins import print
+
+date = '2017-3-23'
+url = 'http://flights.ctrip.com/booking/WNZ-HGH-day-1.html?DDate1='+date
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-headers= {'User-Agent' : user_agent }
+headers = {'User-Agent' : user_agent }
 
 request=urllib.request.Request(url)
 response=urllib.request.urlopen(request)
@@ -12,16 +15,16 @@ print(html)
 
 content = response.read().decode('utf-8')
 pattern = re.compile(r'<div.*?flight_logo">(.*?)</strong.*?span>(.*?)</span.*?'
-			+'flight_logo">(.*?)</strong.*?span>(.*?)</span.*?'
-			+'time">(.*?)</strong.*?<div>(.*?)</div.*?'
-			+'stay-time">(.*?)</span.*?city-name">(.*?)</span.*?'
-			+'time">(.*?)</strong>(.*?)<div>(.*?)</div.*?'
-			+'0">(.*?)</span.*?0">(.*?)</span.*?</dfn>(.*?)</span')
+                     + 'flight_logo">(.*?)</strong.*?span>(.*?)</span.*?'
+                     + 'time">(.*?)</strong.*?<div>(.*?)</div.*?'
+                     + 'stay-time">(.*?)</span.*?city-name">(.*?)</span.*?'
+                     + 'time">(.*?)</strong>(.*?)<div>(.*?)</div.*?'
+                     + '0">(.*?)</span.*?0">(.*?)</span.*?</dfn>(.*?)</span')
 
 items = re.findall(pattern,content)
 for item in items:
-	nextday = re.search("+.*?天" , item[9])
-	if not nextday:
-		print"%s%s ,%s , %s%s , %s , %s(%s)->%s(%s) , %s(%s) , ￥%s"(item[0],item[1],item[11],item[2],item[3],item[12],item[5],item[4],item[10],item[8],item[7],item[6],item[13])
+	next_day = re.search("+.*?", item[9])
+	if not next_day:
+		print("%s%s ,%s , %s%s , %s , %s(%s)->%s(%s) , %s(%s) , ￥%s"%(item[0], item[1], item[11], item[2] , item[3],item[12],item[5],item[4],item[10],item[8],item[7],item[6],item[13]))
 	else:
-		print"%s%s ,%s , %s%s , %s , %s(%s)->%s(%s)%s , %s(%s) , ￥%s"(item[0],item[1],item[11],item[2],item[3],item[12],item[5],item[4],item[10],nextday,item[8],item[7],item[6],item[13])
+		print("%s%s ,%s , %s%s , %s , %s(%s)->%s(%s)%s , %s(%s) , ￥%s"%(item[0], item[1], item[11], item[2], item[3], item[12], item[5], item[4], item[10], next_day, item[8], item[7], item[6], item[13]))
